82bdb7679f9f389fd7d6b3d63ec4c4fc2356e2be
Chance Mims <chance@Chances-MacBook-Air.local>
2020-01-27 17:04:09 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let thankYou = [];
+    for (let counter = 0; counter < names.length; counter++){
+        thankYou.push(`Thank you, ${names[counter]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou;
+}
+
+function countDown(startingNumber){
+    while (startingNumber >= 0){
+        console.log(startingNumber);
+        startingNumber --;
+    }
+}
